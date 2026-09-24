52649de85d1fa6689be48379ef44041be49d0e09
Kocsis Gergely <kocsisg@example.com>
2026-09-24 09:17:53 +0200

    Second commit

--- src/main/java/hu/unideb/inf/App.java
+++ src/main/java/hu/unideb/inf/App.java
@@ -21,6 +21,7 @@ public class App extends Application {
         stage.setScene(scene);
         stage.show();
         //First change
+        //Second change
     }
 
     static void setRoot(String fxml) throws IOException {
